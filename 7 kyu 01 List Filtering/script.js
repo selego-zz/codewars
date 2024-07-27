5305a6e4a865659978c3d966ec2cead6f71d59d0
Bruno Marti Luis <selego@gmail.com>
2024-07-27 19:29:06 +0200

    [UPDATED]7 kyu 01 List Filtering test updated

--- 7 kyu 01 List Filtering/script.js
+++ 7 kyu 01 List Filtering/script.js
@@ -14,20 +14,33 @@ function filter_list(l) {
   return l.filter(element => typeof (element) === "number");
 }
 
+class Test {
+  constructor(initial, expected) {
+    this.initial = initial;
+    this.expected = expected;
+  }
+
+  set test(test) {
+    this._test = test;
+  }
+  get test() {
+    return this._test;
+  }
+  compare() {
+    if (this._test.length !== this.expected.length) return false;
+
+    this._test.forEach(element => { if (!this.expected.includes(element)) return false; });
+    return true;
+  }
+}
 let test = [
-  { initial: [1, 2, 'a', 'b'], expected: [1, 2] },
-  { initial: [1, 'a', 'b', 0, 15], expected: [1, 0, 15] },
-  { initial: [1, 2, 'aasf', '1', '123', 123], expected: [1, 2, 123] },
+  new Test([1, 2, 'a', 'b'], [1, 2]),
+  new Test([1, 'a', 'b', 0, 15], [1, 0, 15]),
+  new Test([1, 2, 'aasf', '1', '123', 123], [1, 2, 123]),
 ];
 
-test.forEach(text => {
-  let result = filter_list(text.initial);
-  console.log(`test: '${text.initial}', obtenido '${result}', esperado '${text.expected}', test ${compare(result, text.expected) ? "Passed" : "Failed"}`);
+test.forEach(element => {
+  element.test = filter_list(element.initial);
+  console.log(`test: '${element.initial}', obtenido '${element.test}', esperado '${element.expected}', test ${element.compare() ? "Passed" : "Failed"}`);
 });
 
-function compare(a1, a2) {
-  if (a1.length !== a2.length) return false;
-
-  a1.forEach(element => { if (!a2.includes(element)) return false; });
-  return true;
-}
